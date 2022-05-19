89933cc5a912ea2b4ab6aa9319ece39a38474644
maoxiaoxing <930649054@qq.com>
2022-05-19 08:08:47 +0800

    尾递归

--- dynamic/509. 斐波那契数.js
+++ dynamic/509. 斐波那契数.js
@@ -2,10 +2,25 @@
  * @param {number} n
  * @return {number}
  */
+
+// 动态规划
+// var fib = function(n) {
+//   const dp = [0, 1]
+//   for (let i = 2; i <= n; i++) {
+//     dp[i] = dp[i-1] + dp[i-2]
+//   }
+//   return dp[n]
+// };
+
+
+// 尾递归
 var fib = function(n) {
-  const dp = [0, 1]
-  for (let i = 2; i <= n; i++) {
-    dp[i] = dp[i-1] + dp[i-2]
+  const dfs = (_n) => {
+      if (_n === 0) return 0
+      if (_n === 1) return 1
+
+      return dfs(_n - 1) + dfs(_n - 2)
   }
-  return dp[n]
+  return dfs(n)
 };
+
