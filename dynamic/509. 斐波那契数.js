1e3477e4df5aeedaaaff7610cafc70acff0650db
maoxiaoxing <930649054@qq.com>
2022-05-19 08:26:14 +0800

    动态规划

--- dynamic/509. 斐波那契数.js
+++ dynamic/509. 斐波那契数.js
@@ -24,15 +24,31 @@
 // };
 
 
+// var fib = function(n) {
+//   // 递归缓存
+//   const helper = (memo, n) => {
+//       if(n<2) return n
+//       if (memo[n]) return memo[n]
+//       memo[n] = helper(memo, n-1) + helper(memo, n-2)
+//       return memo[n]
+//   }
+//   const memo = []
+//   return helper(memo, n)
+// };
+
+
+// 动态规划 优化空间
 var fib = function(n) {
-  // 递归缓存
-  const helper = (memo, n) => {
-      if(n<2) return n
-      if (memo[n]) return memo[n]
-      memo[n] = helper(memo, n-1) + helper(memo, n-2)
-      return memo[n]
+  let dp0 = 0
+  let dp1 = 1
+  if (n === dp0) return dp0
+  if (n === dp1) return dp1
+  let res = dp0 + dp1
+  for (let i = 2; i <= n; i++) {
+      res = dp0 + dp1
+      dp0 = dp1
+      dp1 = res
   }
-  const memo = []
-  return helper(memo, n)
+  return res
 };
 
