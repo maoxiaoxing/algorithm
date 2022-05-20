4b9246361ce5b2ecf59f5fa4afe6801e6f5096c8
maoxiaoxing <930649054@qq.com>
2022-05-20 08:34:22 +0800

    广度优先搜索

--- tree/104. 二叉树的最大深度.js
+++ tree/104. 二叉树的最大深度.js
@@ -21,3 +21,25 @@ var maxDepth = function(root) {
 
 
 
+
+var maxDepth = function(root) {
+  /*
+    广度优先搜索
+    一层层遍历
+    一层为为一组 即左右子树为一组， len 为 1或 2 根或者只有一个子树为 1 ，两个子树为 2
+    遍历完一层 count + 1 代表有一层 
+  */
+  let count = 0
+  if (!root) return count
+  const queue = [root]
+  while (queue.length) {
+      let len = queue.length
+      while (len--) {
+          const node = queue.shift()
+          node.left && queue.push(node.left)
+          node.right && queue.push(node.right)
+      }
+      count+=1
+  }
+  return count
+};
