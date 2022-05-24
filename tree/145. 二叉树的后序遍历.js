6496b1da9612eb8bbf82b1216a297048e3f0e364
maoxiaoxing <930649054@qq.com>
2022-05-24 09:20:02 +0800

    二叉树的后序遍历

---
+++ tree/145. 二叉树的后序遍历.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var postorderTraversal = function(root) {
+  const res = []
+  const dfs = (_root) => {
+      if (!_root) return
+      dfs(_root.left)
+      dfs(_root.right)
+      res.push(_root.val)
+  }
+  dfs(root)
+  return res
+};
+
+
