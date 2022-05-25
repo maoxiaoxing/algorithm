1ab86f9a5c2c22474fdb1ef574d92afe86493d67
maoxiaoxing <930649054@qq.com>
2022-05-25 08:22:12 +0800

    回溯

--- tree/findNodePaths(查找树节点路径).js
+++ tree/findNodePaths(查找树节点路径).js
@@ -1,32 +1,32 @@
 const treeArr = [
   { 
-    id: 01,
+    id: '01',
     pid: null,
     children: [
       { 
-        id: 03,
-        pid: 01,
+        id: '03',
+        pid: '01',
         children: [
-          { id: 04, pid: 03 },
-          { id: 06, pid: 03 },
+          { id: '04', pid: '03' },
+          { id: '06', pid: '03' },
         ]
       },
-      { id: 05, pid: 01 },
+      { id: '05', pid: '01' },
     ]
   },
   { 
-    id: 02, 
+    id: '02', 
     pid: null,
     children: [
       { 
-        id: 07,
-        pid: 02,
+        id: '07',
+        pid: '02',
         children: [
-          { id: 10, pid: 07 },
-          { id: 11, pid: 07 },
+          { id: '10', pid: '07' },
+          { id: '11', pid: '07' },
         ]
       },
-      { id: 09, pid: 02 },
+      { id: '09', pid: '02' },
     ]
   },
 ]
@@ -42,17 +42,14 @@ const findNodePaths = (_tree, _id) => {
       }
       if (node.children) {
         const paths = dfs(node.children, id)
-        console.log(paths, 'paths')
         if (paths && paths.length) return paths
       }
       stack.pop()
     }
     return []
   }
-  dfs(_tree, _id)
-  console.log(stack, 'stack')
-  return stack
+  return dfs(_tree, _id)
 }
 
-console.log(findNodePaths(treeArr, 07))
+console.log(findNodePaths(treeArr, '04'))
 
