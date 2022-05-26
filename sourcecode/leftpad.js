34c60387c80c17f90e334b2605721dc2b72bf53c
maoxiaoxing <930649054@qq.com>
2022-05-26 09:04:41 +0800

    leftPad

--- sourcecode/leftpad.js
+++ sourcecode/leftpad.js
@@ -7,7 +7,44 @@ function leftPad (str, length, ch) {
 }
 
 
-const str = 'hello'
-console.log(leftPad(str, 6, '-'))
+function leftPad1(str, length, ch) {
+  /*
+    -
+    --
+    ----
+    --------
+    二分法
+  */
+  if (str.length > length) return str
+  let len = length - str.length
+  let total = ''
+  while (true) {
+    if (len & 1) {
+      total += ch
+    }
+    if (len <= 1) {
+      return total + str
+    }
+    ch += ch
+    len = len >> 1
+  }
+}
 
-console.log(str.padStart(6, '-'))
+const str = 'hello'
+// console.log(leftPad(str, 6, '-'))
+// console.log(leftPad1(str, 10, '-'))
+
+// console.log(str.padStart(6, '-'))
+
+console.time('leftPad')
+for (let i = 0; i < 10000; i++) {
+  leftPad(str, 100000, '-')
+}
+console.timeEnd('leftPad')
+
+console.time('leftPad1')
+for (let i = 0; i < 10000; i++) {
+  leftPad1(str, 100000, '-')
+}
+console.timeEnd('leftPad1')
+
