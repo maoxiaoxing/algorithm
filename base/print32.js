8320d468bbe026ba90ebe469a810feeab72c2bf2
maoxiaoxing <930649054@qq.com>
2022-06-05 18:26:07 +0800

    位原酸

--- base/print32.js
+++ base/print32.js
@@ -8,6 +8,18 @@ function print(num) {
 }
 // print(4)
 // print(-1)
-print(Number.MAX_SAFE_INTEGER)
-print(-2)
-console.log(~-2)
+// print(Number.MAX_SAFE_INTEGER)
+// print(-2)
+// console.log(~-2)
+
+const a = 123
+const b = 456
+print(a)
+print(b)
+console.log('=======')
+// | 有真（1）则真
+print(a|b)
+// & 全真（1）则真
+print(a&b)
+// 相同为假，不同为真
+print(a^b)
